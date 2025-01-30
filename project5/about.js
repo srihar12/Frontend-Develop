6516c81f602a96942278f5812ea76fdd5dfa7290
Sriharini.M <163291451+srihar12@users.noreply.github.com>
2025-01-30 20:45:28 +0530

    Create about.js

---
+++ project5/about.js
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-Commerce Website</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <section id="header">
+        <a href="#"><img src="img/logo.png" class="logo"></a>
+        <div>
+           <ul id="navbar">
+            <li><a href="index.html">Home</a></li>
+            <li><a  href="shop.html">Shop</a></li>
+            <li><a href="blog.html">Blog</a></li>
+            <li><a class="active" href="about.html">About</a></li>
+            <li><a href="contact.html">Contact</a></li>
+            <li><a href="cart.html"><i class="fa-solid fa-cart-shopping"></i></a></li>
+           </ul>
+        </div>
+    </section>
+    <section id="page-header" class="about-header">
+       <h1>#KnowUs</h1>
+       <p>Lorem ipsum dolor sit amet, consectetur</p>
+        
+    </section>
+    <section id="about-head" class="section-p1">
+         <img src="img/about/a6.jpg" alt="">
+        <div>
+            <h2>Who We Are?</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio obcaecati non sapiente earum eius repudiandae animi voluptatem, magni ipsa eligendi quae molestiae fuga distinctio? Rerum iusto eum explicabo officiis ipsa!
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae repellat tenetur, ullam mollitia aperiam et harum porro doloribus nulla nesciunt quaerat iste, aut quia rem dicta voluptate sint sit quos.
+            </p>
+            <abbr title="">Create stunning images with as much or as little control as you like
+                a choice of basic and creative modes.
+            </abbr>
+            <br>
+            <marquee bgcolor="#ccc" loop="-1" scrollamount="5" width="100%">Create stunning images with as much or as little control as you like
+                a choice of basic and creative modes.</marquee>
+        </div>
+    </section>
+    <section id="about-app" class="section-p1">
+        <h1>Download our  <a href="#"> App</a> </h1>
+        <div class="video1">
+            <video autoplay muted loop src="img/about/1.mp4"></video>
+        </div>
+    </section>
+    <section id="feature" class="section-p1">
+        <div class="fe-box">
+            <img src="img/f1.png">
+            <h6>Free Shipping</h6>
+        </div>
+        <div class="fe-box">
+            <img src="img/f2.png">
+            <h6>Online Order</h6>
+        </div>
+        <div class="fe-box">
+            <img src="img/f3.png">
+            <h6>Save Money</h6>
+        </div>
+        <div class="fe-box">
+            <img src="img/f4.png">
+            <h6>Promotions</h6>
+        </div>
+        <div class="fe-box">
+            <img src="img/f5.png">
+            <h6>Happy sell</h6>
+        </div>
+        <div class="fe-box">
+            <img src="img/f6.png">
+            <h6>F24/7 Support</h6>
+        </div>
+
+    </section>
+    <section id="newsletter" class="section-p1 section-m1">
+        <div class="newstext">
+          <h4>Sign Up For Newsletters</h4>
+          <p>Get E-mail updates about our latest shop and  <span>Special offers.</span></p>
+        </div>
+        <div class="form">
+          <input type="text" placeholder="Your email address">
+          <button class="normal">Sign Up</button>
+        </div>
+  </section>
+    <footer class="section-p1">
+        <div class="col">
+            <img class="logo" src="img/logo.png" alt="">
+            <h4>Content</h4>
+            <p><strong>Address:</strong>562 Wellington Road,Street 32,San Francisco</p>
+            <p><strong>Phone:</strong>+01 2222 365 /(+91) 01 2345 6894</p>
+            <p><strong>Hours:</strong>10:00 -18:00,Mon-Sat</p>
+            <div class="follow">
+                <h4>Follow us</h4>
+                <div class-icon>
+                    <i class="fab fa-facebook-f"></i>
+                    <i class="fab fa-twitter"></i>
+                    <i class="fab fa-instagram"></i>
+                    <i class="fab fa-pinterest-p"></i>
+                    <i class="fab fa-youtube"></i>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <h4>About</h4>
+            <a href="#">About us</a>
+            <a href="#">Delivery Information</a>
+            <a href="#">Privacy Policy</a>
+            <a href="#">Terms & Conditions</a>
+            <a href="#">Contact Us</a>
+        </div>
+        <div class="col">
+            <h4>My Account</h4>
+            <a href="#">Sign In</a>
+            <a href="#">View Cart</a>
+            <a href="#">My Wishlist</a>
+            <a href="#">Track My Order</a>
+            <a href="#">Help</a>
+        </div>
+        <div class="col install">
+            <h4>Install App</h4>
+            <p>From App Store or Google Play</p>
+            <div class="row">
+                <img src="img/app.jpg">
+                <img src="img/play.jpg">
+            </div>
+            <p>Secured Payment Gateways</p>
+            <img src="img/pay.png">
+        </div>
+        <div class="copyright">
+            <p>2021, Tech2 etc-HTML CSS Ecommerce template</p>
+        </div>
+
+    </footer>
+    <script src="main.js"></script>
+</body>
+</html>
